3772b5ded1ecb29dfb44d244a1f111495b08e564
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-08-02 18:51:53 -0400

    validacion del formulario registrarUsuario, email unico y hash passwords

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from "express-validator";
 import Usuario from "../models/Usuarios.js";
 
 export const formularioLogin = (req, res) => {
@@ -13,9 +14,56 @@ export const formularioRegistro = (req, res) => {
 };
 
 export const registrarUsuario = async (req, res) => {
-  console.log(req.body);
-  const usuario = await Usuario.create(req.body);
-  res.json(usuario);
+  //validando campos del formulario(enlasando a traves del name en el input)
+  await check("nombre")
+    .notEmpty()
+    .withMessage("El nombre es obligatorio")
+    .run(req);
+
+  await check("email").isEmail().withMessage("No es un email valido").run(req);
+  await check("password")
+    .isLength({ min: 6 })
+    .withMessage("el password debe ser minimo de 6 caracteres")
+    .run(req);
+
+  await check("repetir_password")
+    .equals(req.body.password)
+    .withMessage("Los Passwords no son iguales")
+    .run(req);
+
+  let resultado = validationResult(req);
+  //validando que no haya errores en el validationResult
+  if (!resultado.isEmpty()) {
+    return res.render("auth/registro", {
+      pagina: "Crear Cuenta",
+      errores: resultado.array(),
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email,
+      },
+    });
+  }
+
+  // si pasa la validadcion verficar email unico
+  const existeUsuario = await Usuario.findOne({
+    where: { email: req.body.email },
+  });
+  if (existeUsuario) {
+    return res.render("auth/registro", {
+      pagina: "Crear Cuenta",
+      errores: [{ msg: "El usuario ya esta registrado con ese email" }],
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email,
+      },
+    });
+  }
+
+  //agregar usuario
+  // const usuario = await Usuario.create(req.body);
+  // res.json(usuario);
+  const { nombre, email, password } = req.body;
+  await Usuario.create({ nombre, email, password, token: 123 });
 };
 
 export const formularioOlvidePassword = (req, res) => {
